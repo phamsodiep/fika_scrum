5c3f8565ec58be5b2e93010dd6c539625a53b891
phamsodiep <phamduchuy@hotmail.com>
2020-06-20 22:07:26 +0700

    [src][fe][MEM](1h) design home page layout with material ui

--- prjs/fika_scrum_fe/src/pages/homepage.js
+++ prjs/fika_scrum_fe/src/pages/homepage.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import { ApplicationBar, AsideMenu } from './homepagecomponents.js';
 
 
-export default class HomePage extends React.Component {
-    render() {
-        return <div>Home page</div>
-    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  }
+}));
+
+export default function HomePage(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+        <CssBaseline />
+        <ApplicationBar open={open} onOpen={handleDrawerOpen} />
+        <AsideMenu open={open} onClose={handleDrawerClose}>
+            <Divider />
+            List1
+
+            <Divider />
+            List2
+        </AsideMenu>
+    </div>
+  );
 }
 
---
+++ prjs/fika_scrum_fe/src/pages/homepagecomponents.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+
+
+const drawerWidth = 240;
+
+const useAppBarStyles = makeStyles((theme) => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  toolbar: {
+    paddingRight: 24,
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  }
+}));
+
+const useAsideMenuStyles = makeStyles((theme) => ({
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  }
+}));
+
+export function ApplicationBar(props) {
+  const classes = useAppBarStyles();
+  return (
+      <AppBar position="absolute" className={clsx(classes.appBar, props.open && classes.appBarShift)}>
+          <Toolbar className={classes.toolbar}>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={props.onOpen}
+                className={clsx(classes.menuButton, props.open && classes.menuButtonHidden)}
+              >
+                  <MenuIcon />
+              </IconButton>
+          </Toolbar>
+      </AppBar>
+  );
+}
+
+export function AsideMenu(props) {
+  const classes = useAsideMenuStyles();
+  return (
+      <aside>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: clsx(classes.drawerPaper, !props.open && classes.drawerPaperClose),
+            }}
+            open={props.open}
+          >            
+              <div className={classes.toolbarIcon}>
+                <IconButton onClick={props.onClose}>
+                  <ChevronLeftIcon />
+                </IconButton>
+              </div>
+              {props.children}
+          </Drawer>
+      </aside>
+  );
+}
+
